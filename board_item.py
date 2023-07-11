b31198ebcb69c4e9816b6937467af4c90df0d79b
stiliamm <redemption.records@icloud.com>
2023-07-11 14:35:05 +0300

    added logging functionality

--- board_item.py
+++ board_item.py
@@ -1,42 +1,67 @@
-from datetime import date, timedelta
 from item_status import ItemStatus
-
-
-def add_days_to_now(d):
-    if d < 0:
-        raise ValueError('Date should not be in the past')
-    return date.today() + timedelta(days=d)
+from datetime import date
+from event_log import EventLog
 
 
 class BoardItem:
     
-    def __init__(self, title: str, due_date: int):
-        self.title = title
-        if self.title == '':
-            raise ValueError('Tile must NOT be empty!')
-        if len(self.title) < 5 or len(self.title) > 30:
+    def __init__(self, title: str, due_date: date):
+        self._title = title
+        self._due_date = due_date
+        self._status = ItemStatus.OPEN
+        self._event_logs = []
+
+    @property
+    def status(self):
+        return self._status
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if len(value) < 5 or len(value) > 30:
             raise ValueError('Title must have more that 5 characters and less that 30.')
-        self.due_date = add_days_to_now(due_date)
-        self.status = ItemStatus.OPEN
-
+        else:
+            previous_title = self._title
+            self._title = value
+            self._add_event_log(f"Title changed from {previous_title} to {value}")
+            print('Ok')
     
+    @property
+    def due_date(self):
+        return self._due_date
+    
+    @due_date.setter
+    def due_date(self, value):
+        if value < date.today():
+            raise ValueError('Due date cant be in the past.')
+
+        previous_due_date = self._due_date
+        self._due_date = value
+        self._add_event_log(f"DueDate changed from {previous_due_date} to {value}")
+
+
     def revert_status(self):
-        start_position = self.status
-        previous_postition = ItemStatus.previous(start_position)
-        self.status = previous_postition
-        return self.status
-        
-    
+        previous_status = self._status
+        self._status = ItemStatus.previous(self._status)
+        self._add_event_log(f"Status changed from {previous_status} to {self._status}")
+
+
     def advance_status(self):
-        start_postiton = self.status
-        next_position = ItemStatus.next(start_postiton)
-        self.status = next_position
-        return self.status
-       
-    
+        previous_status = self._status
+        self._status = ItemStatus.next(self._status)
+        self._add_event_log(f"Status changed from {previous_status} to {self._status}")
+        
     def info(self):
-        return f'{self.title}, [{self.status} | {self.due_date}]'
+        return f'{self._title}, [{self._status} | {self._due_date}]'
+   
 
+    def history(self):
+        return '\n'.join(log.info() for log in self._event_logs)
 
-
+    def _add_event_log(self, description):
+        log = EventLog(description)
+        self._event_logs.append(log)
     
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from board_item import BoardItem
 from board import Board
+from datetime import date, timedelta
+from event_log import EventLog
+
+
+def add_days_to_now(d):
+    if d < 0:
+        raise ValueError('Date should not be in the past')
+    return date.today() + timedelta(days=d)
 
 
 def main():
-    item = BoardItem('Registration doesn\'t work', 2)
-    second_item = BoardItem('Encrypt user data', 10)
+    item = BoardItem('Registration doesn\'t work', add_days_to_now(2))
 
+    item.due_date += timedelta(days=365 * 2)
+    item.title = 'Not that important'
+    item.revert_status()
     item.advance_status()
-
-    board = Board()
-    board.items.append(item)
-    board.items.append(second_item)
-
-    for board_item in board.items:
-        board_item.advance_status()
-
-    for board_item in board.items:
-        print(board_item.info())
+    item.revert_status()
+    print(item.history())
+    
 
 if __name__ == "__main__":
     main()
